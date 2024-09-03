cf672788f6190f3a0b0da458984c624a8d74a830
Dave Siarez <davesiarez18@gmail.com>
2024-09-03 11:01:25 +0800

    auth page design setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,17 @@
 </script>
 
    
-<Button>Button</Button>
+<header class="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
+    <div class="container flex h-14 max-w-screen-2xl items-center">
+        <div class="mr-4 sm:hidden md:hidden lg:flex">
+            <a href="/" class="mr-6 flex items-center space-x-2" > LOGO </a>
+            <nav class="flex items-center gap-6 text-sm">
+                <a href="/" class="hover:text-foreground/80 transition-colors text-foreground/60 capitalize">Test</a>
+                <a href="/" class="hover:text-foreground/80 transition-colors text-foreground/60 capitalize">Test</a>
+                <a href="/auth/login" class="hover:text-foreground/80 transition-colors text-foreground/60 capitalize">Login</a>
+
+            </nav>
+        </div>
+       
+    </div>
+</header>
